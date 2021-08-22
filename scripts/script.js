227b4259ca586bfa8751b70b2781ad528dfa251f
Oleg Gupalo <gupalooleg@gmail.com>
2021-08-22 19:26:48 +0300

    feat: add transformation(keyboard)

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
+const keys = document.querySelectorAll('.key');
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
+document.addEventListener('keydown', playSound);
 
+function playSound(e) {
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!key) return;
+
+  key.classList.add('playing');
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('playing');
+}
