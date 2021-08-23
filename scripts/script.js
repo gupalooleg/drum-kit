04139d491a8ee2e10dfaac03a0b0b056188e4a1a
Oleg Gupalo <gupalooleg@gmail.com>
2021-08-23 15:56:18 +0300

    feat: add dark/light mode

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,8 @@ keys.forEach((key) => {
   key.addEventListener('mousedown', playSound);
 });
 document.addEventListener('keydown', playSound);
+const mode = document.querySelector('.mode');
+mode.addEventListener('click', changeMode);
 
 function playSound(e) {
   if (e.type === 'mousedown' && e.which !== 1) return;
@@ -24,4 +26,8 @@ function playSound(e) {
 function removeTransition(e) {
   if (e.propertyName !== 'transform') return;
   e.target.classList.remove('playing');
+}
+
+function changeMode(e) {
+  document.body.classList.toggle('light');
 }
